0bdb9b90109b3c715162105fe11e1efcd733c896
dreadedLines <246846382+dreadedLines@users.noreply.github.com>
2026-02-10 14:18:08 +0800

    Add home screen

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,33 @@ void main() {
 class FlutterDemoApp extends StatelessWidget {
   const FlutterDemoApp({super.key});
 
+    @override
+    Widget build(BuildContext context) {
+        return MaterialApp(home: const HomeScreen(), debugShowCheckedModeBanner: false,);
+    }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(color: Color(0xFF00FFAA), strokeWidth: 8,);
+    return Scaffold(appBar: AppBar(title: Text("Hello world"), backgroundColor: Color(0xFF000000), foregroundColor: Color(0xAAFFFFFF),), body: Center(child: Text("Hello again", style: const TextStyle(color: Color(0xFFAB00EF), fontFamily: "Roboto", fontSize: 20, shadows: [
+      BoxShadow(
+        color: Color.fromRGBO(0, 0, 0, 0.2),
+        blurRadius: 20,
+        spreadRadius: -7,
+        offset: Offset(0, 60),
+      )
+  ]) ),) ,);
+  }
+}
+
+class DraggableContainer extends StatelessWidget {
+  const DraggableContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
