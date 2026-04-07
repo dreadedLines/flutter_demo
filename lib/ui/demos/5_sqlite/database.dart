57d24e763b71369650c13fb1208e9fa483a78c5a
dreadedLines <246846382+dreadedLines@users.noreply.github.com>
2026-04-07 14:07:41 +0800

    Define onCreate for db

--- lib/ui/demos/5_sqlite/database.dart
+++ lib/ui/demos/5_sqlite/database.dart
@@ -11,8 +11,26 @@ class DatabaseHelper {
   static const columnName = 'name';
   static const columnAge = 'age';
 
+  late Database _database;
+
   Future<void> init() async {
     final folder = await getApplicationDocumentsDirectory();
     final path = join(folder.path, _dbName);
+    Database _db = await openDatabase(
+      path,
+      version: _dbVersion,
+      onCreate: _onCreate,
+    );
+  }
+
+  Future _onCreate(Database db, int version) async {
+    final query = '''
+      CREATE TABLE $table(
+        $columnId INTEGER PRIMARY KEY,
+        $columnName TEXT NOT NULL,
+        $columnAge INTEGER NOT NULL
+      );
+    ''';
+    await db.execute(query);
   }
 }
