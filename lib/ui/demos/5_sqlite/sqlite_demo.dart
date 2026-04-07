3a9f23b641e30a46edf69efa1dd63f204e298a51
dreadedLines <246846382+dreadedLines@users.noreply.github.com>
2026-04-07 14:30:58 +0800

    Add functionality for sqlite_demo

--- lib/ui/demos/5_sqlite/sqlite_demo.dart
+++ lib/ui/demos/5_sqlite/sqlite_demo.dart
@@ -1,3 +1,5 @@
+import 'package:demo/services/service_locator.dart';
+import 'package:demo/ui/demos/5_sqlite/database.dart';
 import 'package:flutter/material.dart';
 
 class SqliteDemo extends StatefulWidget {
@@ -8,6 +10,8 @@ class SqliteDemo extends StatefulWidget {
 }
 
 class _SqliteDemoState extends State<SqliteDemo> {
+  final db = getIt<DatabaseHelper>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,28 +21,44 @@ class _SqliteDemoState extends State<SqliteDemo> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(
-              onPressed: () {
-                
+              onPressed: () async {
+                final row = {
+                  DatabaseHelper.columnName: "Bair",
+                  DatabaseHelper.columnAge: 20,
+                };
+                await db.insert(row);
               }, 
               child: Text("insert")),
             const SizedBox(height: 20,),
             TextButton(
-              onPressed: () {
-                
+              onPressed: () async {
+                final allRows = await db.queryAllRows();
+                debugPrint('query all rows:');
+                for (final row in allRows) {
+                  debugPrint(row.toString());
+                }
               }, 
               child: Text("query"),
               // style: ButtonStyle(),
             ),
             const SizedBox(height: 20,),
             OutlinedButton(
-              onPressed: () {
-                
+              onPressed: () async {
+                Map<String, dynamic> row = {
+                  DatabaseHelper.columnId: 1,
+                  DatabaseHelper.columnName: 'Mary',
+                  DatabaseHelper.columnAge: 32
+                };
+                final rowsAffected = await db.update(row);
+                debugPrint('updated $rowsAffected row(s)');
               }, 
               child: Text("update")),
             const SizedBox(height: 20,),
             TextButton(
-              onPressed: () {
-                
+              onPressed: () async {
+                final id = await db.queryRowCount();
+                final rowsDeleted = await db.delete(id);
+                debugPrint('deleted $rowsDeleted row(s): row $id');
               }, 
               child: Text("delete")),
           ],
