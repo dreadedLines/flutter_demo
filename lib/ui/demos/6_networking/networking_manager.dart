2af7c763da1b53c2af122325f909d243dc99702f
dreadedLines <246846382+dreadedLines@users.noreply.github.com>
2026-04-09 14:04:01 +0800

    Change networking manager input/output

--- lib/ui/demos/6_networking/networking_manager.dart
+++ lib/ui/demos/6_networking/networking_manager.dart
@@ -1,11 +1,37 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart';
+import 'package:path/path.dart';
 
 class NetworkingManager {
+  ValueNotifier catNotifier = ValueNotifier(Map<String, dynamic>);
 
-  void getRequest() {
+  Future<void> getRequest() async {
+    // final uri = Uri.parse("https://www.miu.edu.mn/");
+    // final uri = Uri.parse("https://example.com/api/fetch?limit=10,20,30&max=100");
+    // final uri = Uri.parse("https://jsonplaceholder.typicode.com/posts/1");
+    final uri = Uri.parse("https://catfact.ninja/fact");
+    final response = await get(uri);
 
+    final jsonResponse = response.body;
+    catNotifier = jsonDecode(jsonResponse);
   }
-  void postRequest() {
-
+  Future<void> postRequest() async {
+    final uri = Uri.parse("https://jsonplaceholder.typicode.com/posts");
+      // Map<String, String>? headers,
+      // Object? body,
+      // Encoding? encoding,
+    final headers = {
+      "Accept-Language": "en-US", 
+      "Content-Language": "en-US", 
+      "Content-Type": "application/JSON",
+    };
+    final example = {
+      "name": "Bob",
+      "age": 20,
+      "time": "14:00",
+    };
+    post(uri, headers: headers, body: example, encoding: Encoding.getByName("UTF-8"));
   } 
 }
