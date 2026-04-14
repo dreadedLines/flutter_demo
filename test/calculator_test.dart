2271ac74c786c3b751cfa6628e6dcb63414ebfc8
dreadedLines <246846382+dreadedLines@users.noreply.github.com>
2026-04-14 14:16:32 +0800

    Rename state_management_test, add widget_layour_test

--- test/calculator_test.dart
+++ test/calculator_test.dart
@@ -1,6 +1,4 @@
 import 'package:demo/ui/demos/7_testing/testing_manager.dart';
-import 'package:flutter/material.dart';
-import 'package:demo/ui/demos/3_state_management/state_management_manager.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
@@ -20,5 +18,10 @@ void main() {
     final manager = TestingManager();
     final answer = manager.result.value;
     expect(answer, 0);
+  },);
+  test('calculator test: user inputs invalid value', () {
+    final manager = TestingManager();
+    expect(() => manager.add('abc', '2'), throwsFormatException);
+    expect(() => manager.add('65', 'b'), throwsFormatException);
   },);
 }
