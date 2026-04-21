1b64058b75e37d02a4813939fdf0950c4a084bfa
dreadedLines <246846382+dreadedLines@users.noreply.github.com>
2026-04-21 13:31:56 +0800

    Change line to rectangle

--- lib/ui/demos/9_painting/painting_demo.dart
+++ lib/ui/demos/9_painting/painting_demo.dart
@@ -12,26 +12,34 @@ class _PaintingDemoState extends State<PaintingDemo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: 
         Center(
-          child: CustomPaint( //                       <-- CustomPaint widget
-            size: Size(300, 300),
-            painter: MyPainter(),
-          ),
+          child: Container(
+            decoration: BoxDecoration(border: Border.all(color: Colors.lime)),
+            child: CustomPaint( //                       <-- CustomPaint widget
+              size: Size(300, 300),
+              painter: MyPainter(),
+            ),
+          )
         ),
     );
   }
 }
 
 class MyPainter extends CustomPainter { //         <-- CustomPainter class
+  final paintSettings = Paint()
+  ..color = Colors.black
+  ..style = PaintingStyle.fill
+  ..strokeWidth = 10;
   @override
   void paint(Canvas canvas, Size size) {
-    final p1 = Offset(-100, 0);
-    final p2 = Offset(250, 150);
-    final paint = Paint()
-      ..color = Colors.white
-      ..strokeWidth = 4;
-    canvas.drawLine(p1, p2, paint);
+    final left = -60.0;
+    final top = -100.0;
+    final right = 350.0;
+    final bottom = 200.0;
+    final rect = Rect.fromLTRB(left, top, right, bottom);
+    canvas.drawRect(rect, paintSettings);
   }
   
   @override
